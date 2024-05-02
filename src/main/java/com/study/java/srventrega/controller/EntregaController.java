414b41de9f31576d093d530bda38fb40b1c51555
Diogo Humberto Vitório Dal Bosco <diogo.bosco@solutis.com.br>
2024-05-01 22:53:37 -0300

    preprando estrutura de log integrada logstash

--- src/main/java/com/study/java/srventrega/controller/EntregaController.java
+++ src/main/java/com/study/java/srventrega/controller/EntregaController.java
@@ -1,13 +1,13 @@
 package com.study.java.srventrega.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Log4j2
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class EntregaController {
